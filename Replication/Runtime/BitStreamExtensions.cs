8802f1d3455ceaa3465eba38ee71ea40f5a1346d
unknown <39922492+OneManMonkeySquad@users.noreply.github.com>
2020-07-25 16:40:28 +0200

    Reverted NetworkObject optimization

--- Replication/Runtime/BitStreamExtensions.cs
+++ Replication/Runtime/BitStreamExtensions.cs
@@ -17,28 +17,32 @@ namespace Cube.Replication {
 
 
         public static void WriteNetworkObject(this BitStream bs, NetworkObject networkObject) {
+            bs.Write(networkObject != null ? networkObject.networkAssetId : "");
+
             // #todo this is stupid (32 bytes instead of 16 + could use ranged int because we know how many NetworkObjects (or maybe Ts?) there are)
-            bs.Write(networkObject == null);
-            if (networkObject != null) {
-                for (int i = 0; i < 32; ++i) {
-                    var b = (byte)networkObject.networkAssetId[i];
-                    bs.Write(b);
-                }
-            }
+//             bs.Write(networkObject == null);
+//             if (networkObject != null) {
+//                 for (int i = 0; i < 32; ++i) {
+//                     var b = (byte)networkObject.networkAssetId[i];
+//                     bs.Write(b);
+//                 }
+//             }
         }
 
         public static T ReadNetworkObject<T>(this BitStream bs) where T : NetworkObject {
-            var isEmpty = bs.ReadBool();
-            if (isEmpty)
-                return null;
+            var id = bs.ReadString();
 
-            var chars = new char[32];
-            for (int i = 0; i < 32; ++i) {
-                chars[i] = (char)bs.ReadByte();
-            }
-
-            var id = new string(chars);
-            return (T)NetworkObjectLookup.instance.CreateFromNetworkAssetId(id);
+//             var isEmpty = bs.ReadBool();
+//             if (isEmpty)
+//                 return null;
+// 
+//             var chars = new char[32];
+//             for (int i = 0; i < 32; ++i) {
+//                 chars[i] = (char)bs.ReadByte();
+//             }
+// 
+//             var id = new string(chars);
+             return (T)NetworkObjectLookup.instance.CreateFromNetworkAssetId(id);
         }
     }
 }
