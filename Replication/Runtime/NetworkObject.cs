4c863ad8bf77670d199a251c6233f6bf2ca56597
unknown <39922492+OneManMonkeySquad@users.noreply.github.com>
2021-02-23 17:06:48 +0100

    Refactoring

--- Replication/Runtime/NetworkObject.cs
+++ Replication/Runtime/NetworkObject.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Cube.Replication {
-    [Serializable]
     public class NetworkObject : ScriptableObject {
         [Tooltip("Used when passing the NetworkObject as an RPC argument")]
         public int networkAssetId = -1;
