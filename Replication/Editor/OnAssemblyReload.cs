cb1d9580837779e03ce42c3a5327a269f36ca9db
OneManMonkeySquad <39922492+OneManMonkeySquad@users.noreply.github.com>
2021-05-18 17:24:00 +0200

    Update OnAssemblyReload.cs
    Added Cube/Internal/Force Reload ALL Assemblies button

--- Replication/Editor/OnAssemblyReload.cs
+++ Replication/Editor/OnAssemblyReload.cs
@@ -13,35 +13,40 @@ namespace Cube.Replication.Editor {
         static OnAssemblyReload() {
             CompilationPipeline.assemblyCompilationFinished -= OnCompilationFinished;
             CompilationPipeline.assemblyCompilationFinished += OnCompilationFinished;
+
+            var unityAssemblyPath = Path.GetDirectoryName(EditorApplication.applicationPath);
+            if (unityAssemblyPath.Length == 0 || !Directory.Exists(unityAssemblyPath)) {
+                Debug.LogError("Unity3d assembly path not found (" + unityAssemblyPath + ")");
+                return;
+            }
+
+            var searchPathList = new List<string> {
+                    unityAssemblyPath + "/Data/Managed",
+                    unityAssemblyPath + "/Data/Managed/UnityEngine",
+                    unityAssemblyPath + "/Data/NetStandard/ref/2.0.0",
+                    unityAssemblyPath + "/Data/PlaybackEngines/windowsstandalonesupport/Managed",
+                    unityAssemblyPath + "/Data/UnityExtensions/Unity/Timeline/Editor",
+                    unityAssemblyPath + "/Data/UnityExtensions/Unity/Timeline/RuntimeEditor",
+                    unityAssemblyPath + "/Data/UnityExtensions/Unity/Timeline/Runtime",
+                    Application.dataPath + "/../Library/ScriptAssemblies"
+                };
+
+            resolver = new CachedAssemblyResolver();
+            foreach (var path in searchPathList) {
+                resolver.AddSearchDirectory(path);
+            }
+        }
+
+        [MenuItem("Cube/Internal/Force Reload ALL Assemblies")]
+        static void OnReloadAll() {
+            foreach (var assembly in CompilationPipeline.GetAssemblies(AssembliesType.Editor)) {
+                OnCompilationFinished(assembly.outputPath, null);
+            }
         }
 
         static void OnCompilationFinished(string assemblyPath, CompilerMessage[] messages) {
             if (BuildPipeline.isBuildingPlayer)
                 return;
-
-            if (resolver == null) {
-                var unityAssemblyPath = Path.GetDirectoryName(EditorApplication.applicationPath);
-                if (unityAssemblyPath.Length == 0 || !Directory.Exists(unityAssemblyPath)) {
-                    Debug.LogError("Unity3d assembly path not found (" + unityAssemblyPath + ")");
-                    return;
-                }
-
-                var searchPathList = new List<string> {
-                unityAssemblyPath + "/Data/Managed",
-                unityAssemblyPath + "/Data/Managed/UnityEngine",
-                unityAssemblyPath + "/Data/NetStandard/ref/2.0.0",
-                unityAssemblyPath + "/Data/PlaybackEngines/windowsstandalonesupport/Managed",
-                unityAssemblyPath + "/Data/UnityExtensions/Unity/Timeline/Editor",
-                unityAssemblyPath + "/Data/UnityExtensions/Unity/Timeline/RuntimeEditor",
-                unityAssemblyPath + "/Data/UnityExtensions/Unity/Timeline/Runtime",
-                Application.dataPath + "/../Library/ScriptAssemblies"
-            };
-
-                resolver = new CachedAssemblyResolver();
-                foreach (var path in searchPathList) {
-                    resolver.AddSearchDirectory(path);
-                }
-            }
 
             AssemblyPostProcessor.Start(assemblyPath, resolver);
         }
