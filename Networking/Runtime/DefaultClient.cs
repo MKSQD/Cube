c274b816b599c255c52df6c7df3fc2a3488d85fc
OneManMonkeySquad <39922492+OneManMonkeySquad@users.noreply.github.com>
2021-06-20 22:35:11 +0200

    Added default implementations

---
+++ Networking/Runtime/DefaultClient.cs
@@ -0,0 +1,29 @@
+using Cube.Transport;
+using UnityEngine;
+
+namespace Cube.Networking {
+    [AddComponentMenu("Cube/DefaultClient")]
+    public class DefaultClient : MonoBehaviour {
+        CubeClient client;
+
+        void Start() {
+            var world = new GameObject("Client World");
+            var worldC = world.AddComponent<World>();
+
+            var networkInterface = new LiteNetClientNetworkInterface();
+            networkInterface.ConnectionRequestAccepted += () => Debug.Log($"[Client] Connection request accepted");
+
+            client = new CubeClient(worldC, networkInterface);
+
+            client.networkInterface.Connect("127.0.0.1", 60000);
+        }
+
+        void Update() {
+            client.Update();
+        }
+
+        void OnDestroy() {
+            client.Shutdown();
+        }
+    }
+}
---
+++ Networking/Runtime/DefaultServer.cs
@@ -0,0 +1,39 @@
+using Cube.Replication;
+using Cube.Transport;
+using UnityEngine;
+
+namespace Cube.Networking {
+    public class World : MonoBehaviour, IWorld { }
+
+    [AddComponentMenu("Cube/DefaultServer")]
+    public class DefaultServer : MonoBehaviour {
+        public CubeServer Server;
+
+
+        void Start() {
+            var world = new GameObject("Server World");
+            var worldC = world.AddComponent<World>();
+
+            var networkInterface = new LiteNetServerNetworkInterface(60000);
+            var settings = new ServerReplicaManagerSettings();
+            Server = new CubeServer(worldC, networkInterface, settings);
+
+            networkInterface.NewConnectionEstablished += conn => {
+                Debug.Log($"[Server] New connection {conn}");
+
+                var viewGO = new GameObject($"ReplicaView {conn}");
+                var replicaView = viewGO.AddComponent<ReplicaView>();
+
+                Server.ReplicaManager.AddReplicaView(replicaView);
+            };
+        }
+
+        void Update() {
+            Server.Update();
+        }
+
+        void OnDestroy() {
+            Server.Shutdown();
+        }
+    }
+}
