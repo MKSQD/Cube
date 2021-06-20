15788412c8205951254d55b040eac9592b6cdc91
OneManMonkeySquad <39922492+OneManMonkeySquad@users.noreply.github.com>
2021-06-20 22:57:54 +0200

    Improved ReplicaRigidbody

--- Replication/Runtime/ReplicaRigidbody.cs
+++ Replication/Runtime/ReplicaRigidbody.cs
@@ -13,104 +13,30 @@ namespace Cube.Replication {
         public Transform model;
 
         new Rigidbody rigidbody;
-        bool clientSleeping = false;
 
         const float maxVelocity = 16;
         const float maxAngularVelocity = 16;
 
-        double m_InterpolationBackTime = 0.1;
-        double m_ExtrapolationLimit = 0.5;
-
-        // We store twenty states with "playback" information
-        State[] m_BufferedState = new State[10];
-        // Keep track of what slots are used
-        int m_TimestampCount;
-
-        internal struct State {
-            internal double timestamp;
-            internal Vector3 pos;
-            internal Vector3 velocity;
-            internal Quaternion rot;
-            internal Vector3 angularVelocity;
-        }
+        Vector3 currentModelPosition;
+        Quaternion currentModelRotation;
+        float blend;
 
         void Awake() {
             rigidbody = GetComponent<Rigidbody>();
         }
 
-        void Start() {
-            m_InterpolationBackTime = Replica.settings.DesiredUpdateRate * 2.5f;
-        }
-
         void Update() {
-            if (model == null || !isClient)
-                return;
+            if (model != null) {
+                if (blend < 1) {
+                    blend = Mathf.Min(blend + Time.deltaTime * 3, 1);
 
-#if UNITY_EDITOR
-            if (Replica.isSceneReplica)
-                return;
-#endif
-
-            UpdateTransform();
-        }
-
-        void UpdateTransform() {
-            // This is the target playback time of the rigid body
-            double interpolationTime = Time.timeAsDouble - m_InterpolationBackTime;
-
-            // Use interpolation if the target playback time is present in the buffer
-            if (m_BufferedState[0].timestamp > interpolationTime) {
-                // Go through buffer and find correct state to play back
-                for (int i = 0; i < m_TimestampCount; i++) {
-                    if (m_BufferedState[i].timestamp <= interpolationTime || i == m_TimestampCount - 1) {
-                        // The state one slot newer (<100ms) than the best playback state
-                        State rhs = m_BufferedState[Mathf.Max(i - 1, 0)];
-                        // The best playback state (closest to 100 ms old (default time))
-                        State lhs = m_BufferedState[i];
-
-                        // Use the time between the two slots to determine if interpolation is necessary
-                        double length = rhs.timestamp - lhs.timestamp;
-                        float t = 0.0F;
-                        // As the time difference gets closer to 100 ms t gets closer to 1 in 
-                        // which case rhs is only used
-                        // Example:
-                        // Time is 10.000, so sampleTime is 9.900 
-                        // lhs.time is 9.910 rhs.time is 9.980 length is 0.070
-                        // t is 9.900 - 9.910 / 0.070 = 0.14. So it uses 14% of rhs, 86% of lhs
-                        if (length > 0.0001) {
-                            t = (float)((interpolationTime - lhs.timestamp) / length);
-                        }
-                        //    Debug.Log(t);
-                        // if t=0 => lhs is used directly
-                        transform.localPosition = Vector3.Lerp(lhs.pos, rhs.pos, t);
-                        transform.localRotation = Quaternion.Slerp(lhs.rot, rhs.rot, t);
-                        return;
-                    }
-                }
-            }
-            // Use extrapolation
-            else {
-                State latest = m_BufferedState[0];
-
-                float extrapolationLength = (float)(interpolationTime - latest.timestamp);
-                // Don't extrapolation for more than 500 ms, you would need to do that carefully
-                if (extrapolationLength < m_ExtrapolationLimit) {
-                    float axisLength = extrapolationLength * latest.angularVelocity.magnitude * Mathf.Rad2Deg;
-                    Quaternion angularRotation = Quaternion.AngleAxis(axisLength, latest.angularVelocity);
-
-                    transform.position = latest.pos + latest.velocity * extrapolationLength;
-                    transform.rotation = angularRotation * latest.rot;
-                    rigidbody.velocity = latest.velocity;
-                    rigidbody.angularVelocity = latest.angularVelocity;
-                }
-            }
-        }
-
-        void FixedUpdate() {
-            if (isClient) {
-                if (clientSleeping) {
-                    rigidbody.velocity = Vector3.zero;
-                    rigidbody.angularVelocity = Vector3.zero;
+                    currentModelPosition = Vector3.Lerp(currentModelPosition, transform.position, blend);
+                    currentModelRotation = Quaternion.Lerp(currentModelRotation, transform.rotation, blend);
+                    model.position = currentModelPosition;
+                    model.rotation = currentModelRotation;
+                } else {
+                    currentModelPosition = transform.position;
+                    currentModelRotation = transform.rotation;
                 }
             }
         }
@@ -119,9 +45,12 @@ namespace Cube.Replication {
             bs.Write(transform.position);
 
             var euler = transform.rotation.eulerAngles;
-            if (euler.x < 0) euler.x += 360;
-            if (euler.y < 0) euler.y += 360;
-            if (euler.z < 0) euler.z += 360;
+            if (euler.x < 0)
+                euler.x += 360;
+            if (euler.y < 0)
+                euler.y += 360;
+            if (euler.z < 0)
+                euler.z += 360;
             bs.WriteLossyFloat(euler.x, 0, 360);
             bs.WriteLossyFloat(euler.y, 0, 360);
             bs.WriteLossyFloat(euler.z, 0, 360);
@@ -143,7 +72,7 @@ namespace Cube.Replication {
         }
 
         public override void Deserialize(BitStream bs) {
-            var position = transform.position = bs.ReadVector3();
+            var position = bs.ReadVector3();
 
             var euler = new Vector3 {
                 x = bs.ReadLossyFloat(0, 360),
@@ -154,8 +83,8 @@ namespace Cube.Replication {
 
             var velocity = Vector3.zero;
             var angularVelocity = Vector3.zero;
-            clientSleeping = bs.ReadBool();
-            if (!clientSleeping) {
+            var sleeping = bs.ReadBool();
+            if (!sleeping) {
                 velocity = new Vector3 {
                     x = bs.ReadLossyFloat(-maxVelocity, maxVelocity),
                     y = bs.ReadLossyFloat(-maxVelocity, maxVelocity),
@@ -168,21 +97,15 @@ namespace Cube.Replication {
                 };
             }
 
-            // Shift the buffer sideways, deleting state 20
-            for (int i = m_BufferedState.Length - 1; i >= 1; i--) {
-                m_BufferedState[i] = m_BufferedState[i - 1];
+            transform.position = position;
+            transform.rotation = rotation;
+            rigidbody.velocity = velocity;
+            rigidbody.angularVelocity = angularVelocity;
+            if (sleeping) {
+                rigidbody.Sleep();
             }
 
-            // Record current state in slot 0
-            State state;
-            state.timestamp = Time.timeAsDouble;
-            state.pos = position;
-            state.velocity = velocity;
-            state.rot = rotation;
-            state.angularVelocity = angularVelocity;
-            m_BufferedState[0] = state;
-
-            m_TimestampCount = Mathf.Min(m_TimestampCount + 1, m_BufferedState.Length);
+            blend = 0;
         }
     }
 }
