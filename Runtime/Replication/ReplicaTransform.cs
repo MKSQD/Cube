5fa887b1fb03e6a4cc74af52f8cbe19617ca4e0b
OneManMonkeySquad <39922492+OneManMonkeySquad@users.noreply.github.com>
2022-01-08 14:20:36 +0100

    Improved ReplicaTransform

--- Runtime/Replication/ReplicaTransform.cs
+++ Runtime/Replication/ReplicaTransform.cs
@@ -9,17 +9,14 @@ namespace Cube.Replication {
     /// <remarks>Available in: Editor/Client/Server</remarks>
     [AddComponentMenu("Cube/ReplicaTransform")]
     public class ReplicaTransform : ReplicaBehaviour {
-        // Interpolation
-        double m_InterpolationBackTime = 0.1;
-
         struct State {
-            internal double timestamp;
-            internal Vector3 pos;
-            internal Quaternion rot;
+            public float Timestamp;
+            public Vector3 Position;
+            public Quaternion Rotation;
         }
 
-        State[] m_BufferedState = new State[20];
-        int m_TimestampCount;
+        State[] _bufferedState = new State[20];
+        int _numTimestamps;
 
         public override void Serialize(IBitWriter bs, SerializeContext ctx) {
             bs.WriteVector3(transform.position);
@@ -31,50 +28,36 @@ namespace Cube.Replication {
             var rotation = bs.ReadQuaternion();
 
             // Shift the buffer sideways, deleting state 20
-            for (int i = m_BufferedState.Length - 1; i >= 1; i--) {
-                m_BufferedState[i] = m_BufferedState[i - 1];
+            for (int i = _bufferedState.Length - 1; i >= 1; i--) {
+                _bufferedState[i] = _bufferedState[i - 1];
             }
 
             // Record current state in slot 0
             State state;
-            state.timestamp = Time.timeAsDouble;
-            state.pos = position;
-            state.rot = rotation;
-            m_BufferedState[0] = state;
+            state.Timestamp = Time.time + Replica.SettingsOrDefault.DesiredUpdateRate * 2.5f;
+            state.Position = position;
+            state.Rotation = rotation;
+            _bufferedState[0] = state;
 
-            m_TimestampCount = Mathf.Min(m_TimestampCount + 1, m_BufferedState.Length);
+            _numTimestamps = Mathf.Min(_numTimestamps + 1, _bufferedState.Length);
         }
 
         void Update() {
-            // This is the target playback time of the rigid body
-            double interpolationTime = Time.timeAsDouble - m_InterpolationBackTime;
+            var interpolationTime = Time.time;
 
-            // Use interpolation if the target playback time is present in the buffer
-            if (m_BufferedState[0].timestamp > interpolationTime) {
-                // Go through buffer and find correct state to play back
-                for (int i = 0; i < m_TimestampCount; i++) {
-                    if (m_BufferedState[i].timestamp <= interpolationTime || i == m_TimestampCount - 1) {
-                        // The state one slot newer (<100ms) than the best playback state
-                        State rhs = m_BufferedState[Mathf.Max(i - 1, 0)];
-                        // The best playback state (closest to 100 ms old (default time))
-                        State lhs = m_BufferedState[i];
+            if (_bufferedState[0].Timestamp > interpolationTime) {
+                for (int i = 0; i < _numTimestamps; i++) {
+                    if (_bufferedState[i].Timestamp <= interpolationTime || i == _numTimestamps - 1) {
+                        State rhs = _bufferedState[Mathf.Max(i - 1, 0)];
+                        State lhs = _bufferedState[i];
 
-                        // Use the time between the two slots to determine if interpolation is necessary
-                        double length = rhs.timestamp - lhs.timestamp;
+                        double length = rhs.Timestamp - lhs.Timestamp;
                         float t = 0.0F;
-                        // As the time difference gets closer to 100 ms t gets closer to 1 in 
-                        // which case rhs is only used
-                        // Example:
-                        // Time is 10.000, so sampleTime is 9.900 
-                        // lhs.time is 9.910 rhs.time is 9.980 length is 0.070
-                        // t is 9.900 - 9.910 / 0.070 = 0.14. So it uses 14% of rhs, 86% of lhs
                         if (length > 0.0001) {
-                            t = (float)((interpolationTime - lhs.timestamp) / length);
+                            t = (float)((interpolationTime - lhs.Timestamp) / length);
                         }
-                        //	Debug.Log(t);
-                        // if t=0 => lhs is used directly
-                        transform.localPosition = Vector3.Lerp(lhs.pos, rhs.pos, t);
-                        transform.localRotation = Quaternion.Slerp(lhs.rot, rhs.rot, t);
+                        transform.localPosition = Vector3.Lerp(lhs.Position, rhs.Position, t);
+                        transform.localRotation = Quaternion.Slerp(lhs.Rotation, rhs.Rotation, t);
                         return;
                     }
                 }
