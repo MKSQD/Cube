fb2ee1522774f3d445aef2d8ea93e51c3828abb6
OneManMonkeySquad <39922492+OneManMonkeySquad@users.noreply.github.com>
2022-03-15 23:30:37 +0100

    Refactoring

--- Runtime/Transport/Connection.cs
+++ Runtime/Transport/Connection.cs
@@ -4,38 +4,22 @@ namespace Cube.Transport {
     [Serializable]
     public struct Connection : IEquatable<Connection> {
         public static readonly Connection Invalid = new Connection(unchecked((ulong)-1));
-        
-        public ulong id {
-            get;
-            internal set;
-        }
+
+        public ulong id { get; private set; }
 
         public Connection(ulong id) {
             this.id = id;
         }
 
-        public override string ToString() {
-            return id.ToString();
-        }
+        public override string ToString() => id.ToString();
 
-        public override bool Equals(object other) {
-            return Equals((Connection)other);
-        }
+        public override bool Equals(object other) => Equals((Connection)other);
 
-        public override int GetHashCode() {
-            return (int)(17 + id * 31);
-        }
+        public override int GetHashCode() => (int)(17 + id * 31);
 
-        public bool Equals(Connection other) {
-            return id == other.id;
-        }
+        public bool Equals(Connection other) => id == other.id;
 
-        public static bool operator ==(Connection lhs, Connection rhs) {
-            return lhs.Equals(rhs);
-        }
-
-        public static bool operator !=(Connection lhs, Connection rhs) {
-            return !lhs.Equals(rhs);
-        }
+        public static bool operator ==(Connection lhs, Connection rhs) => lhs.Equals(rhs);
+        public static bool operator !=(Connection lhs, Connection rhs) => !lhs.Equals(rhs);
     }
 }
